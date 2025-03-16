a7316d45ad10d898e2fb882dfe8461a446455c25
vidura Lakshitha <153290869+ViduraLakshitha@users.noreply.github.com>
2025-03-17 04:54:44 +0530

    update

--- frontend/src/component/auction/AuctionCard.jsx
+++ frontend/src/component/auction/AuctionCard.jsx
@@ -1,24 +1,27 @@
 // components/VehicleAuctionCard.js
 
-import React from 'react';
-import CountdownTimer from './CountdownTimer';
+import React from "react";
+import CountdownTimer from "./CountdownTimer";
 
 const AuctionCard = ({ auction }) => {
-    const handleFinish = () => {
-        console.log(`Auction ${auction.title} has finished.`);
-    };
+  const handleFinish = () => {
+    console.log(`Auction ${auction.title} has finished.`);
+  };
 
-    return (
-        <div className="border p-4 my-2 rounded-md shadow-md">
-            <h2>{auction.title}</h2>
-            <p>Initial price: ${auction.initialVehiclePrice}</p>
-            <p>current bid: ${auction.currentBid}</p>
-            <p>Status: {auction.auctionStatus}</p>
-            {auction.auctionStatus === "active" && (
-                <CountdownTimer endTime={new Date(auction.endDateTime).getTime()} onFinish={handleFinish} />
-            )}
-        </div>
-    );
+  return (
+    <div className="border p-4 my-2 rounded-md shadow-md">
+      <h2>{auction.title}</h2>
+      <p>Initial price: ${auction.initialVehiclePrice}</p>
+      <p>current bid: ${auction.currentBid}</p>
+      <p>Status: {auction.auctionStatus}</p>
+      {auction.auctionStatus === "active" && (
+        <CountdownTimer
+          endTime={new Date(auction.endDateTime).getTime()}
+          onFinish={handleFinish}
+        />
+      )}
+    </div>
+  );
 };
 
 export default AuctionCard;
