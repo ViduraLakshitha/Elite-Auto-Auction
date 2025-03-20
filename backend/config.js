60f5f3d9de9aa9b07f74f98fa50d2b82958a7c4a
vidura Lakshitha <153290869+ViduraLakshitha@users.noreply.github.com>
2025-03-21 03:30:47 +0530

    updated

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-// MongoDB connection URL
-export const mongoDBURL = process.env.MONGODB_URI || 'mongodb+srv://your_default_connection_string_here';
-
-//Server port
 export const PORT = 5555;
 
-// JWT secret key
-export const JWT_SECRET = process.env.JWT_SECRET
+//connection string should be here mongodb
+
+export const mongoDBURL = 'mongodb+srv://vidura:0000@elite-auto-auctions.k3nvt.mongodb.net/?retryWrites=true&w=majority&appName=Elite-Auto-Auctions'
