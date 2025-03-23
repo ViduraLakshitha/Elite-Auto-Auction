6521b9570b2b2da8e6170e5bca0512eb1922d4f2
vidura Lakshitha <153290869+ViduraLakshitha@users.noreply.github.com>
2025-03-23 18:04:22 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,8 @@ import buyerScoreboardRoutes from './routes/buyerScoreboardRoutes.js';
 import { Server } from "socket.io";
 import http from "http";
 
-import { Server } from "socket.io";
-import http from "http";
+//import { Server } from "socket.io";
+//import http from "http";
 //test
 
 
