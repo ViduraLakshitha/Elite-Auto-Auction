b452fd193515803288000bc98e4e94b653c9c373
themiya2001 <154418657+themiya2001@users.noreply.github.com>
2025-03-24 17:56:41 +0530

    Remove MongDB URI

--- backend/config.js
+++ backend/config.js
@@ -2,10 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // MongoDB connection URL
-export const mongoDBURL = process.env.MONGODB_URI || 'http://localhost:5555';
+export const mongoDBURL = 'mongodb+srv://vidura:0000@elite-auto-auctions.k3nvt.mongodb.net/?retryWrites=true&w=majority&appName=Elite-Auto-Auctions'
 
 //Server port
 export const PORT = 5555;
 
-// JWT secret key
-export const JWT_SECRET = process.env.JWT_SECRET
+// // JWT secret key
+// export const JWT_SECRET = process.env.JWT_SECRET
